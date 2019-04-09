0d24c9f461720eb27fc90268f31be2c0954a92d6
F9Uf <atoseka@gmail.com>
2019-04-09 15:39:32 +0700

    test auto issue & create jsDoc

--- server/v1/index.js
+++ server/v1/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
+// middle ware
+const permit = require('./middleware/permission');
 
-// controllers 
-const order = require('./controllers/order');
+// controllers
+const authController = require('./controllers/auth.controller');
+// const ordersController = require('./controllers/orders.controller');
+
 
 router.get('/', (req, res) => {
     res.status(200).json({
         success: true,
-        message: 'Api v1 w15151'
+        message: 'Api v2 work!'
     });
 });
 
 
-router.get('/order', order.getOrder);
+// auth
+router.post('/login', authController.login);
+router.post('/signup', authController.signup);
 
+// order
+// @Todo #1 create orders Api
+// router.get('/orders', permit(), ordersController.getOrder);
 
 module.exports = router;
---
+++ server/v1/models/employee.model.js
@@ -0,0 +1,54 @@
+const db = require('../../db');
+
+/**
+ * Create new employee
+ * @param {Object} data
+ * @param {string} data.firstName
+ * @param {string} data.lastName
+ * @param {string} data.addressName
+ * @param {number} data.phone
+ * @param {string} data.email
+ * @param {string} data.password
+ * @param {string} data.shopID
+ * 
+ * @param {requestCallback} done
+ * @callback requestCallback
+ * @param {Object} err
+ * @param {Object} response
+ */
+exports.createNewEmployee = (data, done) => {
+    const sql = 'INSERT INTO employee VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+    db.query(sql, 
+        [data.id, data.firstName,data.lastName, data.adderss||null, data.phone||null, data.email||null, data.position, data.salary, data.username, data.password, data.shopID], 
+        (err, res) => {
+        if (err) {
+            done(err, null);
+        } else {
+            done(null, res);
+        }
+    });
+}
+
+exports.findUserByUsername = (data, done) => {
+    const sql1 = 'SELECT EmployeeID, Username, Password, Position FROM employee WHERE Username = ?';
+
+    db.query(sql1, [data.username], (err, res) => {
+        if (err) {
+            done(err, null);
+        } else {
+            done(null, res);
+        }
+    });
+}
+
+exports.findUserById = (data, done) => {
+    const sql = 'SELECT * FROM employee WHERE EmployeeID = ?'
+
+    db.query(sql, [data.id], (err, res) => {
+        if (err) {
+            done(err, null);
+        } else {
+            done(null, res);
+        }
+    });
+}
