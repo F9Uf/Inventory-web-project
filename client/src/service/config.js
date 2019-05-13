ee3f53a941f593efcfed5276c404e33d6cebbd60
F9Uf <atoseka@gmail.com>
2019-05-13 18:49:15 +0700

    update endpoint on fronted

--- client/src/service/config.js
+++ client/src/service/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    VUE_APP_API_BASE_URL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:5000/api/v1'
+    VUE_APP_API_BASE_URL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:5000/api'
 }
