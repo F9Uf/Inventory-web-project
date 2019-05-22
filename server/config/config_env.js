96118159c96f4ef674588cb2bcecc3a9b592f5f5
F9Uf <atoseka@gmail.com>
2019-05-22 18:49:02 +0700

    add cloud db config

--- server/config/config_env.js
+++ server/config/config_env.js
@@ -1,8 +1,8 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 5000,
-  DB_host: process.env.DB_host || 'localhost',
-  DB_user : process.env.DB_user || 'root',
-  DB_password : process.env.DB_password || '',
-  DB_database : process.env.DB_database || 'inventory_database2'
+  DB_host: process.env.DB_host || 'us-cdbr-iron-east-03.cleardb.net',
+  DB_user : process.env.DB_user || 'b241da87ca3d3b',
+  DB_password : process.env.DB_password || '3c118caa',
+  DB_database : process.env.DB_database || 'heroku_784b4884b79ec0d'
 }
