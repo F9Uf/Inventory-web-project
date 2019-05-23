9d2d23bed620eadeeedbfccc999db842231ea018
pongpat11 <pongpatsitiputa@gmail.com>
2019-05-23 14:07:18 +0700

    d

--- server/api/index.js
+++ server/api/index.js
@@ -12,7 +12,7 @@ const employeeUpdateOne = require('./employees/employeeUpdateOne')
 
 //const car
 const allCar = require('./car/allCar')
-const addOneCar = require('./car/addOneCar')
+const addOneCar = require('./car/adOneCar')
 
 router.get('/', (req, res) => {
   res.status(200).json({
