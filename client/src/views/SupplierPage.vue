5d41dad3ee39e820adfefbc6bf9db1ea06743e6b
F9Uf <atoseka@gmail.com>
2019-05-23 18:07:43 +0700

    add supplierpage

---
+++ client/src/views/SupplierPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout>
+    <h3>Suppler Information</h3><br>
+  </layout>
+</template>
+
+<script>
+import layout from './LAYOUT'
+
+export default {
+  components: {
+    layout
+  }
+}
+</script>
+
+<style>
+
+</style>
