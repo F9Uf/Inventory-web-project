3a2a382c862dc98eb5a3f22a35cadcc57f726d27
neconone <p.chaiolan30@gmail.com>
2019-05-23 15:54:19 +0700

    delete sup complete

--- server/api/suppliers/deleteOneSup.js
+++ server/api/suppliers/deleteOneSup.js
@@ -2,7 +2,7 @@ const db = require('../../db');
 
 module.exports = (req, res) => {
     const id = req.params.supplierID;
-    const sql = 'DELETE FROM car WHERE supplierID = ?'
+    const sql = 'DELETE FROM supplier WHERE supplierID = ?'
     
     db.query(sql, [id], (err, data) => {
         if(err) {
