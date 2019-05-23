1ebb5c0682ed1c904fb35eb2677a6e09f6790c3e
F9Uf <atoseka@gmail.com>
2019-05-23 12:13:35 +0700

    add template

---
+++ client/src/views/template.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row m-3" v-if="userSetting">
+    <div class="col bg-white p-3">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
