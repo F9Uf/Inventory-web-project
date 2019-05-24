064e7e2f25ba4201ea9a596b4e32e9b2dcfe4ccd
Somprasit <keima2541@gmail.com>
2019-05-24 11:00:37 +0700

    udate shippingpage /edit button

--- client/src/views/ShippingPage.vue
+++ client/src/views/ShippingPage.vue
@@ -1,14 +1,27 @@
 <template>
     <layout>
         <h3>Shipping</h3>
-        <the-table :header="header" :body="body"  @onDelete="deleteData" @onEdit="editData" id="ShippingID"></the-table>
-
-        <the-modal v-if="showModal" @close="showModal = false">
+        <the-table :header="header" :body="body"  @onDelete="deleteData" @onEdit="editData" id="shippingID"></the-table>
+        <h5 v-if="!body">No Shipping</h5>
+        <the-modal v-if="showModal" @close="showModal = false" @update="updateData">
           <template v-slot:header>
             <h5>Edit Shipping</h5>
           </template>
           <template v-slot:body>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem nisi voluptates iste labore recusandae? Ad eum in adipisci impedit veritatis exercitationem esse, debitis, incidunt dolor laborum, quia eveniet quo quasi!
+            <div class="form-row">
+              <div class="col">
+                <label >Shipping ID</label>
+                <input type="number" class="form-control" v-model="editShipping.shippingID">
+              </div>
+              <div class="col">
+                <label >Car ID</label>
+                <input type="number" class="form-control" v-model="editShipping.carID">
+              </div>
+              <div class="col">
+                <label >Driver</label>
+                <input type="number" class="form-control" v-model="editShipping.employeeID">
+              </div>
+            </div>
           </template>
         </the-modal>
 
@@ -28,29 +41,43 @@ export default {
   data() {
     return {
       header: ['Shipping ID', 'Car ID','Driver'],
-      body: [{
-        shipid:'0001',carid: '00000002', driver: '00001'
-      }],
+       body:null 
+      // { shipid:'0001',carid: '00000002', driver: '00001'
+      // }
+      ,
+      editShipping: {},
       showModal: false
     }
   },
   create() {
-    // this.fetchShipping()
+    this.fetchShipping()
   },
   methods: {
     deleteData (value) {
+      $api({ path: `/shipping/${value}`, method: `delete`})
+      .then(data => {
+        this.fetchShipping()
+      })
       console.log(value)
     },
     editData (value) {
       this.showModal = true
+      this.editShipping = JSON.parse(stringtify(this.body.fliter(e => e.shippingID ===value)[0]))
     },
     // waiting for api
-    // fatchShipping () {
-    //   $api({path: '/shipping', method: 'get'})
-    //   .then( dara => {
-    //     this.body = data.result
-    //   })
-    // }
+    fatchShipping () {
+      $api({path: '/shipping', method: 'get'})
+      .then( dara => {
+        this.body = data.result
+      })
+    },
+    updateData () {
+      $api({ path: `/shipping/${this.editShipping.shippingID}`, method: 'put', data: this.editShipping})
+      .then( data => {
+        this.showModal = false
+        this.fetchShipping()
+      })
+    }
     
   }
 
