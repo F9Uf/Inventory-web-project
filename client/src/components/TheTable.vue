0c656405f8332daa5df36411caa672bf850702f5
F9Uf <atoseka@gmail.com>
2019-05-24 10:33:48 +0700

    edit table component

--- client/src/components/TheTable.vue
+++ client/src/components/TheTable.vue
@@ -1,28 +1,32 @@
 <template>
-  <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col" v-for="(head, hi) in header" :key="hi">{{head}}</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(b, bi) in body" :key="bi">
-      <th scope="row">{{bi}}</th>
-      <template v-for="(a, i) in b" >
-        <td :key="i" v-if="isImage(a)">
-          <img :src="a" width="80px" >
-        </td>
-        <td :key="i" v-else>{{a}}</td>
-      </template>
-      <td>
-        <button type="button" class="btn btn-danger" @click="$emit('onDelete', b[id])">Delete</button> |
-        <button type="button" class="btn btn-warning" @click="$emit('onEdit', b[id])">Edit</button>
-      </td>
-    </tr>
-  </tbody>
-</table>
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col" v-for="(head, header_index) in header" :key="header_index">{{head}}</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(b, body_index) in body" :key="body_index" @click="$emit('onSelect',b[id])">
+          <th scope="row">{{body_index + 1}}</th>
+          <template v-for="(a, i) in b" >
+            <td :key="i" v-if="isImage(a)">
+              <img :src="a" width="80px" >
+            </td>
+            <td :key="i" v-else>{{a}}</td>
+          </template>
+          <td>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <button type="button" class="btn btn-danger" @click="$emit('onDelete', b[id])">Delete</button>
+              <button type="button" class="btn btn-warning" @click="$emit('onEdit', b[id])">Edit</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
