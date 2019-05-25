748f2899db6015a2507c8f6b48f7b1d7e8fa1305
F9Uf <atoseka@gmail.com>
2019-05-25 13:31:25 +0700

    add base-table component

---
+++ client/src/components/BaseTable.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col" v-if="hasIndex">#</th>
+          <th scope="col" v-for="(head, header_index) in header" :key="header_index">{{head.label}}</th>
+          <th scope="col" v-if="hasAction">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, body_index) in body" :key="body_index">
+          <th scope="row" v-if="hasIndex">{{body_index + 1}}</th>
+
+          <template v-for="(attribute, a_index) in header">
+            <td :key="a_index">{{item[attribute.name]}}</td>
+          </template>
+
+          <th v-if="hasAction">
+            <slot :row-id="item[idName]"></slot>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: Array,
+    body: Array,
+    hasAction: {
+      type: Boolean,
+      default: false
+    },
+    hasIndex: {
+      type: Boolean,
+      default: true
+    },
+    idName: String,
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- client/src/views/AboutPage.vue
+++ client/src/views/AboutPage.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <layout>
+    <base-table :header="header" :body="data" :hasAction="true" idName="employeeID" :hasIndex="true">
+      <!-- haseAction คือบอกว่าจะมี column action (colum สุดท้าย) ไหม เป็น true/false -->
+      <!-- hasIndex คือบอกว่าจะให้แสดง column index หน้าสุดหรือไม่ เป็น true/false -->
+      <!-- idName คือกำหนดว่า จะให้ attribute ไหนของ body เป็น id หลัก-->
+      <button class="btn btn-outline-warning" slot-scope="row" @click="editData(row.rowId)">edit</button>
+      <!-- ใส่ได้มากกว่า 1 ปุ่ม -->
+    </base-table>
+  </layout>
 </template>
+
+<script>
+import Layout from './LAYOUT'
+import BaseTable from '../components/BaseTable'
+
+export default {
+  components: {
+    BaseTable, Layout
+  },
+  data() {
+    return {
+      // header เป็น array ของ json อยากให้ column ไหนขึ้นก่อนก็จัดลำดับใน header ได้เลย
+      // อยากแสดงแค่ column ไหน ก็กำหนด header ไว้แค่นั้น เช่นในนี้ เอาแค่ employeeID กับ firstName
+      // label คือ ชื่อใหม่ที่อยากเปลี่ยน
+      header: [
+        {
+          name: 'employeeID',
+          label: 'Employee ID',
+        },
+        {
+          name: 'firstName',
+          label: 'First Name'
+        }
+      ],
+
+      // data เอาไปใส่ในส่วนของ body ปกติเหมือนเดิม
+      data: [
+        {firstName: 'tose', employeeID: '01', lastName: '1'},
+        {firstName: 'bom', employeeID: '02', lastName: '2'},
+        {firstName: 'ton', employeeID: '03', lastName: '3'},
+        {firstName: 'karn', employeeID: '04', lastName: '4'},
+        {firstName: 'big', employeeID: '05', lastName: '5'},
+      ]
+    }
+  },
+  methods: {
+    editData (id) {
+      console.log(id)
+    }
+  },
+}
+</script>
