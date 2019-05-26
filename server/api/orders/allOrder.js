10822ee69a49124f365d6c5a853e1a9e69300e47
tonzazahaha <monkey33429@gmail.com>
2019-05-26 15:44:13 +0700

    ddd

--- server/api/orders/allOrder.js
+++ server/api/orders/allOrder.js
@@ -1,9 +1,9 @@
 const db = require('../../db');
 
 module.exports = (req, res) => {
-  const sql = `select o.orderID, o.createAt, o.orderType, o.shopID, s.shopName
-  FROM order o LEFT JOIN shop s
-  ON o.shopID = s.shopID;`
+  const sql = `SELECT o.orderID,o.createAt,o.orderType,o.shopID,s.shopName,od.status 
+  FROM order o,orderdetail od,shop s 
+  WHERE o.orderID = od.orderID AND o.shopID = s.shopID`
   db.query(sql, (err, data) => {
     if (err) {
       return res.json({
