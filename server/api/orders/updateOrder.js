18dd57bf60a85341874025b0b35a3fbdbc21e484
tonzazahaha <monkey33429@gmail.com>
2019-05-26 16:01:41 +0700

    fin allorder

--- server/api/orders/updateOrder.js
+++ server/api/orders/updateOrder.js
@@ -1,26 +1,23 @@
 const db = require('../../db');
+
 module.exports = (req, res) => {
-  const item = req.params.item_id; 
-  const sql = `SELECT i.itemID,i.itemName,SUM(o.itemCount) AS totalCount,i.weight,i.area,i.supplierID,o.locationID FROM item i LEFT JOIN orderdetail o ON i.itemID = o.itemID WHERE i.itemID = ? GROUP BY o.itemID`
-  db.query(sql,[item],(err,data) => {
+    const catId= req.params.category_id;
+    const catName = req.body.catName;
+    const sql = 'UPDATE category SET categoryName = ? WHERE categoryID = ?'
+
+
+  db.query(sql,[catName,catId], (err, data) => {
     if (err) {
+        console.log(err)
       return res.json({
         success: false,
-        message: err
+        message: 'Update category is error!'
       })
     } else {
-      if (data && data[0]) {
-        return res.json({
-          success: true,
-          message: 'Successfully found the items',
-          result: data
-        })
-      } else {
-        return res.json({
-          success: false,
-          message: 'There are no items'
-        })
-      }
+      return res.json({
+        success: true,
+        message: 'Update category is successful!'
+      })
     }
   })
 }
