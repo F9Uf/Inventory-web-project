a8cd87b530e28869c83657a957b3c9fc693e9efd
pongpat11 <pongpatsitiputa@gmail.com>
2019-05-27 13:39:17 +0700

    label area to volume

--- client/src/views/Cars/CarPage.vue
+++ client/src/views/Cars/CarPage.vue
@@ -85,7 +85,7 @@ export default {
         { name: 'model', label: 'Model'},
         { name: 'licensePlate', label: 'License Plate'},
         { name: 'carWeight', label: 'Weight (KG)'},
-        { name: 'carArea', label: 'Area (m3)'},
+        { name: 'carArea', label: 'Volume (m3)'},
         { name: 'carStatus', label: 'Status'}
       ],
       body: [],
