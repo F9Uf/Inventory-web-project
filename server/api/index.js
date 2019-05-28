1f81f128f2f4a2a77c43d09134c27e67d34c461c
neconone <p.chaiolan30@gmail.com>
2019-05-28 20:57:10 +0700

    order

--- server/api/index.js
+++ server/api/index.js
@@ -58,7 +58,7 @@ const updatePosition = require('./positions/updatePosition');
 //order
 const allOrder = require('./orders/allOrder');
 const updateOrder = require('./orders/updateOrder');
-//const addOrder = require('./orders/addOrder');
+//const addOrder = require('./orders/insertComplexOrder');
 
 
 //location
@@ -143,7 +143,7 @@ router.put('/positions/:position_id', permit(),updatePosition);
 //orders
 router.get('/orders',permit(),allOrder);
 router.put('/orders/:order_id',permit(),updateOrder);
-//router.post('/orders',permit(),addOrder);
+//router.post('/orders',permit(),insertComplexOrder);
 
 
 //location
