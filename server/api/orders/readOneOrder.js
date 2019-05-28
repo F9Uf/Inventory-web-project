03cf24365d269e141bdca7bef6481aba888ef0a9
neconone <p.chaiolan30@gmail.com>
2019-05-28 22:38:05 +0700

    readOneOrder Fix query

--- server/api/orders/readOneOrder.js
+++ server/api/orders/readOneOrder.js
@@ -3,14 +3,15 @@ const conditionQuery = require('../../services/queryToStringCondition');
 
 module.exports = (req, res) => {
     const orderID = req.params.order_id;
-    let sql = 'SELECT * FROM ordermain o LEFT JOIN orderdetail od ON o.orderID = od.orderID LEFT JOIN shop s ON o.shopID = s.shopID ';
+    let sql = 'SELECT * FROM ordermain o LEFT JOIN orderdetail od ON o.orderID = od.orderID LEFT JOIN shop s ON o.shopID = s.shopID';
     let condition = conditionQuery(req.query);
     let value_arr = [];
+    console.log(condition.sql);
     if (condition) {
-        sql += condition.sql + 'AND ';
+        sql += condition.sql + ' AND ';
         value_arr = condition.value_array;
     }
-    sql += 'orderID = ?';
+    sql += 'o.orderID = ?';
     value_arr.push(orderID);
     db.query(sql,  value_arr, (err, data) => {
         if (err) {
