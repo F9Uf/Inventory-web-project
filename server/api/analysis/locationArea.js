459f7cd49ec4eb5a1ef21ff13b45d7046889cfc1
neconone <p.chaiolan30@gmail.com>
2019-05-29 15:52:53 +0700

    fix locationArea

--- server/api/analysis/locationArea.js
+++ server/api/analysis/locationArea.js
@@ -1,17 +1,14 @@
 const db = require('../../db');
 
 module.exports = (req, res) => {
-    const sql = `  SELECT a.locationID, a.locationName, s.stockName, ROUND((a.maxArea - IF(area > 0, area, 0)), 2) AS area
+    const sql = `  SELECT a.locationID, a.locationName, s.stockName, ROUND((a.maxArea - IF(area > 0, area, 0)), 2) AS leftArea
         FROM ( 
         SELECT l.locationID, l.locationName, l.stockID, l.maxArea, SUM(i.area * od.itemCount) as area 
         FROM orderdetail od
         RIGHT JOIN location l ON od.locationID = l.locationID
         LEFT JOIN item i ON od.itemID = i.itemID
         GROUP BY l.locationID
-    ) as a LEFT JOIN stock s ON a.stockID = s.stockID;
-    select *
-    from item
-    where itemID = 000000000000006;`
+    ) as a LEFT JOIN stock s ON a.stockID = s.stockID;`
     
     db.query(sql, (err, data) => {
         if (err) {
