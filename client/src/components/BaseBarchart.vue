5c0f719b49266db4929af5068f1395212400f918
Somprasit <keima2541@gmail.com>
2019-05-29 12:52:27 +0700

    kuy

--- client/src/components/BaseBarchart.vue
+++ client/src/components/BaseBarchart.vue
@@ -7,11 +7,20 @@ import { Bar } from 'vue-chartjs'
 
 export default {
   extends: Bar,
-  props: ['chartdata', 'options'],
+  props: {
+    chartdata: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
   mounted () {
     this.renderChart(this.chartdata, this.options)
-  }
-
+  },
+  
 }
 </script>
 
