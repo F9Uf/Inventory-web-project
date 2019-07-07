8457a049b88df028328ecad41b6cd0e8765b50c3
F9Uf <33904716+F9Uf@users.noreply.github.com>
2019-07-07 17:00:51 +0700

    change db host name

--- server/config/config_env.js
+++ server/config/config_env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 5000,
-  DB_host: process.env.DB_host || 'http://localhost/',
+  DB_host: process.env.DB_host || 'localhost',
   DB_user : process.env.DB_user || '',
   DB_password : process.env.DB_password || '',
   DB_database : process.env.DB_database || 'inventory'
