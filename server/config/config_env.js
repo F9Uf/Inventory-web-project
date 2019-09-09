d6ed9f72a59daafa1c76c6e688729436d9b349e4
F9Uf <atoseka@gmail.com>
2019-09-09 22:26:30 +0700

    implement running on docker (db&server)

--- server/config/config_env.js
+++ server/config/config_env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 5000,
   DB_host: process.env.DB_host || 'localhost',
-  DB_user : process.env.DB_user || '',
-  DB_password : process.env.DB_password || '',
+  DB_user : process.env.DB_user || 'root',
+  DB_password : process.env.DB_password || 'root',
   DB_database : process.env.DB_database || 'inventory'
 }
